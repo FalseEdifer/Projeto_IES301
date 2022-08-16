d0d8bb14feeaa15aa68fd0a007d6fa7fd861bd7a
Ingrid <71271942+ing01@users.noreply.github.com>
2022-08-16 12:12:13 -0300

    Update dia.php

--- dia.php
+++ dia.php
@@ -10,7 +10,6 @@
         monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
         var data = new Date();
         document.write ("Hoje é " + dayName[data.getDay() ] + ", " + data.getDate () + " de " + monName[data.getMonth()] + " de "  + data.getFullYear())
-</script>
     </script>
 </head>
 <body>
